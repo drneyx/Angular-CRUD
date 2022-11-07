36e911e7b9c91f86bf719fc02c3b37f2813432cc
Nathani Luving <cosmasnathan9@gmail.com>
2022-11-07 17:09:24 +0300

    Form reset

--- src/app/dialog/dialog.component.ts
+++ src/app/dialog/dialog.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { ApiService } from '../services/api.service';
+import {MatDialogRef} from '@angular/material/dialog';
 
 @Component({
   selector: 'app-dialog',
@@ -11,7 +13,7 @@ export class DialogComponent implements OnInit {
   freshnessList = ["Brand New", "Second Hand", "Refurbrished"];
   productForm !: FormGroup;
 
-  constructor(private formBuilder : FormBuilder) { }
+  constructor(private formBuilder : FormBuilder, private api : ApiService, private dialogRef : MatDialogRef<DialogComponent>) { }
 
   ngOnInit(): void {
     this.productForm = this.formBuilder.group({
@@ -25,7 +27,19 @@ export class DialogComponent implements OnInit {
   }
 
   addProduct(){
-    console.log(this.productForm.value)
+   if(this.productForm.valid){
+    this.api.postProduct(this.productForm.value)
+    .subscribe({
+      next: (res) => {
+        alert("Product added successfully!");
+        this.productForm.reset();
+
+      },
+      error:(err) =>{
+        alert("Error while adding product");
+      }
+    })
+   }
   }
 
 
